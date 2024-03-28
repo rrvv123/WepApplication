5e6663be1b1ef5ae38da708e2c8f44caf284ced1
rrvv123 <eorms7391@gmail.com>
2024-03-28 11:52:30 +0900

    Thymeleaf 환경설정

---
+++ src/main/java/kr/ac/kopo/wepapplication/WepApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.wepapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WepApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/ac/kopo/wepapplication/WepApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.wepapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
